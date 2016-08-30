2dac188784439b59c541f84a3a24ca9b3c4c4903
rcmdnk <rcmdnk@gmail.com>
2016-08-30 16:30:11 +0900

    update

--- bin/getFacebook.py
+++ bin/getFacebook.py
@@ -3,15 +3,25 @@
 import requests
 import datetime
 import time
+import re
 
 SITE_ROOT = "http://rcmdnk.github.io"
 URL_LIST = SITE_ROOT + "/urllist.html"
 MAX_TRY = 100
 WAIT_TIME = 300
+URL_PATTERNS = ["/blog/20\d\d/\d\d/\d\d/.+"]
 
 print "Start at %s" % datetime.datetime.today()
 
-urls = requests.get(URL_LIST).text.split()
+patterns = [re.compile(r"%s" % x) for x in URL_PATTERNS]
+urls_tmp = requests.get(URL_LIST).text.split()
+urls = []
+for u in urls_tmp:
+    for p in patterns:
+        if p.match(u):
+            urls.append(u)
+            break
+
 n = len(urls)
 
 print "Total %d urls" % n
@@ -19,16 +29,25 @@ print "Total %d urls" % n
 shares = {}
 
 for u in urls:
+    if u == "/404.html":
+        break
     print "%d urls remaining..." % n
     n -= 1
     for i in xrange(MAX_TRY):
         if i != 0:
             print "Waiting %d times" % i
-        f = requests.get("http://graph.facebook.com/?id=%s%s" % (SITE_ROOT, u))
-        j = f.json()
-        if "share" in j:
-            shares[u] = j["share"]["share_count"]
-            break
+        try:
+            print "http://graph.facebook.com/?id=%s%s" % (SITE_ROOT, u)
+            f = requests.get("http://graph.facebook.com/?id=%s%s"
+                             % (SITE_ROOT, u))
+            j = f.json()
+            if "share" in j:
+                shares[u] = j["share"]["share_count"]
+                break
+        except:
+            print "Can't get json for %s" % u
+
+        print 'waiting..'
         time.sleep(WAIT_TIME)
 
 with open("facebookshare.txt") as f:
